34f4d6289f676b2ad4bb79c6c3140aee09f99b6f
thiliporace <85422792+thiliporace@users.noreply.github.com>
2023-05-09 20:57:21 +0000

    Initial commit

---
+++ GrapplingRope.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrapplingRope : MonoBehaviour
+{
+    private Spring spring;
+    private LineRenderer lr;
+    private Vector3 currentGrapplePosition;
+    public Grappling gp;
+    public Transform gunTip;
+    public int quality;
+    public float damper;
+    public float strength;
+    public float velocity;
+    public float waveCount;
+    public float waveHeight;
+    public AnimationCurve affectCurve;
+    
+
+    private void Awake()
+    {
+        lr = GetComponent<LineRenderer>();
+        spring = new Spring();
+        spring.SetTarget(0);
+    }
+
+    private void LateUpdate()
+    {
+        DrawRope();
+    }
+    
+    private void DrawRope()
+    {
+        if (!gp.grappling)
+        {
+            currentGrapplePosition = gunTip.position;
+            spring.Reset();
+            if (lr.positionCount > 0)
+                lr.positionCount = 0;
+            return;
+        }
+
+        if (lr.positionCount == 0)
+        {
+            spring.SetVelocity(velocity);
+            lr.positionCount = quality + 1;
+        }
+        
+        spring.SetDamper(damper);
+        spring.SetStrength(strength);
+        spring.Update(Time.deltaTime);
+
+        var grapplePoint = gp.grapplePoint;
+        var gunTipPosition = gp.gunTip.position;
+        var up = Quaternion.LookRotation(grapplePoint - gunTipPosition).normalized * Vector3.up;
+
+        currentGrapplePosition = Vector3.Lerp(currentGrapplePosition, gp.grapplePoint, Time.deltaTime * 12f);
+
+        for (int i = 0; i < quality + 1; i++)
+        {
+            var delta = i / (float)quality;
+            var offset = up * waveHeight * Mathf.Sin(delta * waveCount * Mathf.PI) * spring.Value *
+                         affectCurve.Evaluate(delta);
+            
+            lr.SetPosition(i, Vector3.Lerp(gunTipPosition,currentGrapplePosition,delta) + offset);
+        }
+    }
+}
